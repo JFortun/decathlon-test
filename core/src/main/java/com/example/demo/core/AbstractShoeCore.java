d7433fd1a8b09774fe6405873180271b304b0bde
JFortun <josemanuelfortunromero@gmail.com>
2022-10-11 17:19:30 +0200

    Change Lombok's val for Java's var

--- core/src/main/java/com/example/demo/core/AbstractShoeCore.java
+++ core/src/main/java/com/example/demo/core/AbstractShoeCore.java
@@ -1,7 +1,6 @@
 package com.example.demo.core;
 
 import com.example.demo.facade.ShoeFacade;
-import lombok.val;
 import org.springframework.beans.FatalBeanException;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,7 +15,7 @@ public abstract class AbstractShoeCore implements ShoeCore {
     @PostConstruct
     void init() {
 
-        val version = Optional.ofNullable(this.getClass().getAnnotation(Implementation.class))
+        final var version = Optional.ofNullable(this.getClass().getAnnotation(Implementation.class))
                 .map(Implementation::version)
                 .orElseThrow(() -> new FatalBeanException("AbstractShoeCore implementation should be annotated with @Implementation"));
 
